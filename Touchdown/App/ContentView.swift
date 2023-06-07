820f1d235bd6d3a649718f277494813cf1d347c8
Atash Musazada <a.musazada@gmail.com>
2023-06-07 16:37:07 +0400

    Initial Commit

---
+++ Touchdown/App/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  Touchdown
+//
+//  Created by Atash Musazada on 07.06.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    //MARK: - PROPERTY
+    
+    //MARK: - BODY
+    
+    var body: some View {
+        
+        ZStack {
+            VStack(spacing: 0) {
+                NavigationBarView()
+                    .padding(.horizontal, 15)
+                    .padding(.bottom)
+                    .padding(.top, UIApplication
+                        .shared
+                        .connectedScenes
+                        .flatMap { ($0 as? UIWindowScene)?.windows ?? [] }
+                        .first { $0.isKeyWindow }?.safeAreaInsets.top)
+                    .background(Color.white)
+                    .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)
+                
+                Spacer()
+                
+                FooterView()
+                    .padding(.horizontal)
+            }//: VSTACK
+            .background(colorBackground.ignoresSafeArea(.all, edges: .all))
+        }//: ZSTACK
+        .ignoresSafeArea(.all, edges: .top)
+    }
+}
+
+//MARK: - PREVIEW
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Touchdown/App/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Atash Musazada on 07.06.23.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
