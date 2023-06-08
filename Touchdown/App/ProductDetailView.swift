de1dfbd006437e764cc6c63aa0aa01451d0da72f
Atash Musazada <a.musazada@gmail.com>
2023-06-08 16:05:37 +0400

    develop a product detail view

---
+++ Touchdown/App/ProductDetailView.swift
@@ -0,0 +1,76 @@
+//
+//  ProductDetailView.swift
+//  Touchdown
+//
+//  Created by Atash Musazada on 08.06.23.
+//
+
+import SwiftUI
+
+struct ProductDetailView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 5, content: {
+            //NAVBAR
+            NavigationBarDetailView()
+                .padding(.top, UIApplication
+                                    .shared
+                                    .connectedScenes
+                                    .flatMap { ($0 as? UIWindowScene)?.windows ?? [] }
+                                    .first { $0.isKeyWindow }?.safeAreaInsets.top)
+            
+            //HEADER
+            HeaderDetailView()
+                .padding(.horizontal)
+            
+            //DETAIL TOP Part
+            TopPartDetailView()
+                .padding(.horizontal)
+                .zIndex(1)
+            
+            //DETAIL BOTTOM BAR
+            VStack(alignment: .center, spacing: 0, content: {
+                //RATINGS + SIZE
+
+                RatingSizesDetailView()
+                    .padding(.top, -20)
+                    .padding(.bottom, 20)
+                
+                //DESCRIPTION
+                ScrollView(.vertical, showsIndicators: false, content: {
+                    Text(sampleProduct.description)
+                        .font(.system(.body, design: .rounded))
+                        .foregroundColor(.gray)
+                        .multilineTextAlignment(.leading)
+                })//: SCROOL
+
+                //QUANTITY + FAVORITE
+                QuantityFavoriteDetailView()
+                    .padding(.vertical, 10)
+
+                //ADD TO CART
+                AddToCartDetailView()
+                    .padding(.bottom, 20)
+
+            })//:VSTACK
+            .padding(.horizontal)
+            .background(
+                Color.white
+                    .clipShape(CustomShape())
+                    .padding(.top, -105)
+            )
+        })//: VSTACK
+        .zIndex(0)
+        .ignoresSafeArea(.all, edges: .all)
+        .background(
+            Color(red: sampleProduct.red, green: sampleProduct.green, blue: sampleProduct.blue)
+                .ignoresSafeArea(.all, edges: .all)
+        )
+    }
+}
+
+struct ProductDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductDetailView()
+            .previewLayout(.fixed(width: 375, height: 812))
+    }
+}
---
+++ Touchdown/View/Detail/NavigationBarDetailView.swift
@@ -0,0 +1,39 @@
+//
+//  NavigationBarDetailView.swift
+//  Touchdown
+//
+//  Created by Atash Musazada on 08.06.23.
+//
+
+import SwiftUI
+
+struct NavigationBarDetailView: View {
+    
+    var body: some View {
+        HStack {
+            Button(action: {}, label: {
+                Image(systemName: "chevron.left")
+                    .font(.title)
+                    .foregroundColor(.white)
+            })
+            
+            Spacer()
+            
+            Button(action: {}, label: {
+                Image(systemName: "cart")
+                    .font(.title)
+                    .foregroundColor(.white)
+            })
+        }//: HSTACK
+        .padding(.horizontal)
+    }
+}
+
+struct NavigationBarDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationBarDetailView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+            .background(Color.gray)
+    }
+}
